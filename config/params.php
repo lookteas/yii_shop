846559a37f38316f17b072fab8e6a9683ba01675
together <tenger@opea.cn>
2018-01-22 11:37:39 +0800

    新增后台会员添加，前台会员登录，第三方QQ登录

--- config/params.php
+++ config/params.php
@@ -4,5 +4,12 @@ return [
     'adminEmail' => 'admin@example.com',
     'pageSize' => [
         'manage' => 10,
+        'user'   => 10,
+        'product' => 10,
+        'frontproduct' => 9,
+        'order' => 10,
+    ],
+    'defaultValue' => [
+        'avatar' => 'assets/admin/img/contact-img.png',
     ],
 ];
--- controllers/MemberController.php
+++ controllers/MemberController.php
@@ -1,17 +1,102 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Tenger
- * Date: 2018/1/19
- * Time: 17:27
- */
-
 namespace app\controllers;
+use app\controllers\CommonController;
+use app\models\User;
+use Yii;
 
-
-class MemberController extends BaseController{
-    public function actionAuth(){
-        $this->layout = "lay02";
-        return $this->render('auth');
+class MemberController extends CommonController
+{
+    public function actionAuth()
+    {
+        $this->layout = 'lay02';
+        if (Yii::$app->request->isGet) {
+            $url = Yii::$app->request->referrer;
+            if (empty($url)) {
+                $url = "/";
+            }
+            Yii::$app->session->setFlash('referrer', $url);
+        }
+        $model = new User;
+        if (Yii::$app->request->isPost) {
+            $post = Yii::$app->request->post();
+            if ($model->login($post)) {
+                $url = Yii::$app->session->getFlash('referrer');
+                return $this->redirect($url);
+            }
+        }
+        return $this->render("auth", ['model' => $model]);
     }
+
+    public function actionLogout()
+    {   
+        Yii::$app->session->remove('loginname');
+        Yii::$app->session->remove('isLogin');
+        if (!isset(Yii::$app->session['isLogin'])) {
+            return $this->goBack(Yii::$app->request->referrer);
+        }
+    }
+
+    public function actionReg()
+    {
+        $model = new User;
+        if (Yii::$app->request->isPost) {
+            $post = Yii::$app->request->post();
+            if ($model->regByMail($post)) {
+                Yii::$app->session->setFlash('info', '电子邮件发送成功');
+            }
+        }
+        $this->layout = 'lay02';
+        return $this->render('auth', ['model' => $model]);
+    }
+
+    public function actionQqlogin()
+    {
+        require_once("../vendor/qqlogin/qqConnectAPI.php");
+        $qc = new \QC();
+        $qc->qq_login();
+    }
+
+    public function actionQqcallback()
+    {
+        require_once("../vendor/qqlogin/qqConnectAPI.php");
+        $auth = new \OAuth();
+        $accessToken = $auth->qq_callback();
+        $openid = $auth->get_openid();
+        $qc = new \QC($accessToken, $openid);
+        $userinfo = $qc->get_user_info();
+        $session = Yii::$app->session;
+        $session['userinfo'] = $userinfo;
+        $session['openid'] = $openid;
+        if ($model = User::find()->where('openid = :openid', [':openid' => $openid])->one()) {
+            $session['loginname'] = $model->username;
+            $session['isLogin'] = 1;
+            return $this->redirect(['index/index']);
+        }
+        return $this->redirect(['member/qqreg']);
+    }
+
+    public function actionQqreg()
+    {
+        $this->layout = "lay02";
+        $model = new User;
+        if (Yii::$app->request->isPost) {
+            $post = Yii::$app->request->post();
+            $session = Yii::$app->session;
+            $post['User']['openid'] = $session['openid'];
+            if ($model->reg($post, 'qqreg')) {
+                $session['loginname'] = $post['User']['username'];
+                $session['isLogin'] = 1;
+                return $this->redirect(['index/index']);
+            }
+        }
+        return $this->render('qqreg', ['model' => $model]);
+    }
+
+
+
+
+
+
+
+
 }
